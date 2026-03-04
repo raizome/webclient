1c03e4022c60813d215a41c957708ff2fa2f76dd
abdul <cseabdulrahim@gmail.com>
2026-03-04 22:13:24 +0100

    chore: sort imports and fix style issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { Toaster as Sonner } from "@src/components/ui/sonner";
-import { TooltipProvider } from "@src/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Dashboard from "@src/pages/Dashboard";
-import TagDetail from "@src/pages/TagDetail";
-import Login from "@src/pages/Login";
-import ArticlePage from "@src/pages/ArticlePage";
-import NotFound from "@src/pages/NotFound";
-import Register from "@src/pages/Register";
+import Dashboard                    from "@src/pages/Dashboard";
+import TagDetail                    from "@src/pages/TagDetail";
+import Login                        from "@src/pages/Login";
+import ArticlePage                  from "@src/pages/ArticlePage";
+import NotFound                     from "@src/pages/NotFound";
+import Register                     from "@src/pages/Register";
+import { Toaster as Sonner }        from "@src/components/ui/sonner";
+import { TooltipProvider }          from "@src/components/ui/tooltip";
+import { QueryClient, QueryClientProvider }
+                                    from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route }
+                                    from "react-router-dom";
 
 const queryClient = new QueryClient();
 
@@ -15,12 +17,15 @@ const App = () => (
     <QueryClientProvider client={queryClient}>
         <TooltipProvider>
             <Sonner />
+
             <BrowserRouter future={{ v7_startTransition: true }}>
                 <Routes>
+                    {/* loads at Dashboard by default */}
                     <Route path="/" element={<Dashboard />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
                     <Route path="/dashboard" element={<Dashboard />} />
+
                     <Route
                         path="/dashboard/tags/:tagName"
                         element={<TagDetail />}
@@ -29,10 +34,11 @@ const App = () => (
                         path="/dashboard/article/:itemId"
                         element={<ArticlePage />}
                     />
-                    {/* ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
+
         </TooltipProvider>
     </QueryClientProvider>
 );
