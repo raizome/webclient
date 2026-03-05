a0663f6542c5c369e7e818993ca51c20ba76e1b5
abdul <89011337+abdulrahim2002@users.noreply.github.com>
2026-03-05 13:05:37 +0000

    fix: add application/json header for post req only

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,9 +3,9 @@ import axios, { AxiosInstance } from "axios";
 const client: AxiosInstance = axios.create({
     baseURL: import.meta.env.VITE_BACKEND_URL,
     timeout: 3000,
-    headers: {
-        "Content-Type": "application/json"
-    }
 });
+
+// data in all post requests shall be sent in JSON
+client.defaults.headers.post["Content-Type"] = "application/json";
 
 export default client;
