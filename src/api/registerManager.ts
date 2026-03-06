8ebbdf8b184bc89bc4d6201843b2a1eb369b2990
abdul <89011337+abdulrahim2002@users.noreply.github.com>
2026-03-06 11:07:49 +0000

    docs: add documentation on registerManaget
    - the register endpoint does not log the user in automatisch

--- src/api/registerManager.ts
+++ src/api/registerManager.ts
@@ -1,3 +1,10 @@
+/**
+ * registerManager handles the API call to the /register endpoint. On successfull registration
+ * the endpoint returns the user information. Note, that it does not log the user in, or
+ * set the session cookie automatically. Therefore the user shall be redirected to /login
+ * after registration at the fronend.
+*/
+
 import client               from "@src/api/client";
 import BACKEND_ENDPOINTS    from "@src/commons/endpoints";
 
